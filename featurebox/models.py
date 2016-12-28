7db0a01ac4cb7f9217bbcfdb62b0093de72603d1
m17505 <d.zannikos@gmail.com>
2016-12-28 08:05:55 +0100

    template updates

--- featurebox/models.py
+++ featurebox/models.py
@@ -3,12 +3,11 @@ from cms.models.pluginmodel import CMSPlugin
 from image_cropping import ImageRatioField, ImageCropField
 from cms.models.fields import PageField
 
-
 class FeatureBox(CMSPlugin):
-    # image = ThumbnailerImageField(upload_to='featurebox', blank=False)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
     image = ImageCropField(upload_to='featurebox', blank=False)
     # size is "width x height"
     cropping = ImageRatioField('image', '360x360')
-    text = models.TextField()
     page_link = PageField(blank=True, null=True)
     url = models.URLField(blank=True, null=True)
--- snjweb/static/js/init.slideshow.js
+++ snjweb/static/js/init.slideshow.js
@@ -1,11 +1,19 @@
 $(document).ready(function () {
     $('.front-slideshow').slick({
     dots: false,
+    infinite: true,
     autoplay: true,
     autoplaySpeed: 8000,
+    speed: 800,
     mobileFirst: false,
     nextArrow: '<i class="fa fa-angle-right"></i>',
     prevArrow: '<i class="fa fa-angle-left"></i>',
+    slide: '.frontslide',
     });
 
+    $('.front-slideshow').on('afterChange', function(event, slick, currentSlide){
+      $('.slide-caption-layer').removeClass('animate-caption');
+      $('.slick-active .slide-caption-layer').addClass('animate-caption');
+    });
+    $('.slick-active .slide-caption-layer').addClass('animate-caption');
 });

8a1e8811a18c1072504c4ac593f4b162510d653d
m17505 <d.zannikos@gmail.com>
2016-12-31 01:32:35 +0100

    change settings for amazon s3

--- snjweb/settings.py
+++ snjweb/settings.py
@@ -220,5 +220,5 @@ if not DEBUG:
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_S3_SECURE_URLS = False  # use http instead of https
+    AWS_S3_SECURE_URLS = True  # use http instead of https
     AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests

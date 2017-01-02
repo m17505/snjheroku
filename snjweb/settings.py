418794c165e3fd66240ffcc636a7519e1e23b934
m17505 <d.zannikos@gmail.com>
2017-01-02 11:10:11 +0100

    change settings for s3

--- snjweb/settings.py
+++ snjweb/settings.py
@@ -159,8 +159,7 @@ ALLOWED_HOSTS = ['*']
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
+
 
 # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (
@@ -172,10 +171,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-
-
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
-MEDIA_URL = "/media/"
 
 CMS_TEMPLATES = (
     ('template_1.html', 'Home Template'),
@@ -214,13 +209,35 @@ CMS_ENABLE_UPDATE_CHECK = False
 IMAGE_CROPPING_BACKEND = 'image_cropping.backends.easy_thumbs.EasyThumbnailsBackend'
 IMAGE_CROPPING_BACKEND_PARAMS = {}
 
+# if not DEBUG:
+#     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_MEDIA_BUCKET_NAME']
+#     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+#     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+#     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+#     AWS_S3_SECURE_URLS = True  # use http instead of https
+#     AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
+#     S3_URL = 'http://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
+#     STATIC_URL = S3_URL
+
 if not DEBUG:
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_S3_SECURE_URLS = True  # use http instead of https
-    AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
-    S3_URL = 'http://%s.s3.amazonaws.com/static/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
+    AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
+    AWS_QUERYSTRING_AUTH = False
+    AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
+
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# STATIC_URL = '/static/'
+# MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+# MEDIA_URL = "/media/"
+
+#static media settings
+STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+MEDIA_URL = STATIC_URL + 'media/'
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static"), )
+STATIC_ROOT = 'staticfiles'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+

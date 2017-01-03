10a13a04f8dc146798228e501f2f004b93816aed
m17505 <d.zannikos@gmail.com>
2017-01-03 13:38:11 +0100

    settings update

--- snjweb/settings_base.py
+++ snjweb/settings_base.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 # Application definition
@@ -94,29 +94,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'snjweb.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ['DB_NAME'],
-#         'USER': os.environ['DB_USER'],
-#         'PASSWORD': os.environ['DB_PASSWORD'],
-#         'HOST': os.environ['DB_HOST'],
-#         'PORT': '5432',
-#     }
-# }
-
-SITE_ID = 1
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
@@ -145,10 +122,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Update database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
@@ -158,13 +131,6 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-
-
-
-# Extra places for collectstatic to find static files.
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_ROOT, 'static'),
-# )
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -208,41 +174,3 @@ THUMBNAIL_PROCESSORS = (
 CMS_ENABLE_UPDATE_CHECK = False
 IMAGE_CROPPING_BACKEND = 'image_cropping.backends.easy_thumbs.EasyThumbnailsBackend'
 IMAGE_CROPPING_BACKEND_PARAMS = {}
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
-
-if not DEBUG:
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_S3_SECURE_URLS = True  # use http instead of https
-    AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
-    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-    STATIC_URL = S3_URL + 'static/'
-    MEDIA_URL = S3_URL + 'media/'
-    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-# if not DEBUG:
-#     DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-#     STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-#     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID", "")
-#     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY", "")
-#     AWS_STORAGE_BUCKET_NAME = os.environ.get("AWS_STORAGE_BUCKET_NAME", "")
-#     AWS_QUERYSTRING_AUTH = False
-#     AWS_S3_SECURE_URLS = True  # use http instead of https
-#     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-#     STATIC_URL = S3_URL + 'static/'
-#     MEDIA_URL = S3_URL + 'media/'
-#     STATICFILES_DIRS = ( os.path.join(PROJECT_ROOT, "static"), )
-#     STATIC_ROOT = 'staticfiles'
-#     ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-
-# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-# STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
-# MEDIA_URL = "/media/"
-
--- snjweb/settings_dev.py
+++ snjweb/settings_dev.py
@@ -1,1 +1,19 @@
-from django.conf.settings import *
+from snjweb.settings_base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+  os.path.join(PROJECT_ROOT, 'static'),
+)
+
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, "media")
+MEDIA_URL = "/media/"
+
+SITE_ID = 2

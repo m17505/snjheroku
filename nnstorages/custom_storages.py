24a8c936e35301991a052be0def090b39110823d
m17505 <d.zannikos@gmail.com>
2017-01-04 19:32:26 +0100

    custom storages

--- nnstorages/custom_storages.py
+++ nnstorages/custom_storages.py
@@ -1,10 +1,10 @@
 # custom_storages.py
-from snjweb.settings import *
-from storages.backends.s3boto import S3BotoStorage
+from django.conf import settings
+# from storages.backends.s3boto import S3BotoStorage
 
 
 class StaticStorage(S3BotoStorage):
-    location = STATICFILES_LOCATION
+    location = settings.STATICFILES_LOCATION
 
 class MediaStorage(S3BotoStorage):
-    location = MEDIAFILES_LOCATION
+    location = settings.MEDIAFILES_LOCATION

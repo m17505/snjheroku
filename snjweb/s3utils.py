49a474ad91e8874c14f1f3404a2a7cc027b61c04
m17505 <d.zannikos@gmail.com>
2017-01-05 08:09:31 +0100

    settings for custom s3 storages

--- snjweb/s3utils.py
+++ snjweb/s3utils.py
@@ -1,9 +1,9 @@
 from storages.backends.s3boto import S3BotoStorage
 
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    AWS_S3_SECURE_URLS = True  # use http instead of https
-    AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_S3_SECURE_URLS = True  # use http instead of https
+AWS_QUERYSTRING_AUTH = False  # don't add complex authentication-related query parameters for requests
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
 MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')

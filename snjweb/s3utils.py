04fac137c8b0d674a01de5f97fe23ac7bfec7c1f
m17505 <d.zannikos@gmail.com>
2017-01-05 09:58:45 +0100

    easy_thumbnails & S3

--- snjweb/s3utils.py
+++ snjweb/s3utils.py
@@ -1,4 +1,6 @@
 from storages.backends.s3boto import S3BotoStorage
 
 StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+# MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+class MediaS3BotoStorage(S3BotoStorage):
+    location = 'media'
